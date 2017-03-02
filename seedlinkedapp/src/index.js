31308c87aca88161270d041255bc8a5c2721da2b
Justyna Foster <justynawfoster@gmail.com>
2017-03-02 13:00:17 -0700

    added material-ui

--- seedlinkedapp/src/index.js
+++ seedlinkedapp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -13,3 +14,7 @@ ReactDOM.render(
     <App />
   </Provider>
   , document.querySelector('.container'));
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
