93e2d464ba557ac1bc52cc34565c6d7ff0171d88
Justyna Foster <justynawfoster@gmail.com>
2017-03-02 15:02:19 -0700

    initial react setup

--- seedlinkedapp/src/index.js
+++ seedlinkedapp/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 
 import App from './components/app';
-import reducers from './reducers';
+// import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider>
     <App />
   </Provider>
-  , document.querySelector('.container'));
+  , document.getElementById('container'));
 
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+
