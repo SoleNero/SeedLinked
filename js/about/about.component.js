4146d1ad6326e8904b700b52738c22b4c9b0e5d0
Justyna Foster <justynawfoster@gmail.com>
2017-03-08 22:47:38 -0700

    style update var profile

--- js/about/about.component.js
+++ js/about/about.component.js
@@ -7,12 +7,11 @@
 
   function controller(){
     const vm = this;
-      console.log("you're in about component in controller'");
+
     
     vm.$onInit = onInit;
 
     function onInit(){
-      console.log("you're in about component in oninit'");
     };
   }
 }());
